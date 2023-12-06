00b1f94b2e6083623e1b67b9975e99d4f83b5311
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 02:58:05 +0530

    code added

---
+++ 08. Basic Problems/01_HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+Problem Description:
+Print "Hello World !" in the output
+
+Output Format:
+Print in a single line “Hello World !
+
+Example Output:
+Hello World !
+
+Note - You don’t have to print “” in the output
+*/
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+ 
+        System.out.println("Hello World !");
+    }
+}
