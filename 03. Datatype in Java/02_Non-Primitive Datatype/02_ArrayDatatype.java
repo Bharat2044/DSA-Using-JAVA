6fbcc845447ec9c25c79e5d20460e37699d23bcd
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-06 23:59:42 +0530

    code added

---
+++ 03. Datatype in Java/02_Non-Primitive Datatype/02_ArrayDatatype.java
@@ -0,0 +1,14 @@
+public class ArrayDatatype {
+        
+    public static void main(String args[]) {
+        int []a = {1, 2, 3};    // int a[] = {1, 2, 3};  or  // int[] a = {1, 2, 3};
+        
+        int []b = new int[]{4, 5, 6};
+        
+        System.out.println(a);
+        System.out.println(a[0]);
+        
+        System.out.println(b);
+        System.out.println(b[0]);
+    }
+}
