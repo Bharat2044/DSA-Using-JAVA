8861acfe61eb0f5e63481d8760f1a2e5c05873f5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 02:43:28 +0530

    code added

---
+++ 07. Operators in Java/06_AssignmentOperator.java
@@ -0,0 +1,65 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        // 1. Assignment operator (=)
+        int a = 20;
+        System.out.println(a);
+        
+        // 2. Addition assignment operator (+=)
+        int b = 20;
+        b += 10;
+        System.out.println(b);
+        
+        // 3. Subtraction assignment operator (-=)
+        int c = 20;
+        c -= 10;
+        System.out.println(c);
+        
+        // 4. Multiplication assignment operator (*=)
+        int d = 20;
+        d *= 10;
+        System.out.println(d);
+        
+        // 5. Division assignment operator (/=)
+        int e = 20;
+        e /= 10;
+        System.out.println(e);
+        
+        // 6. Remainder assignment operator (%=)
+        int f = 20;
+        f %= 10;
+        System.out.println(f);
+        
+        // 7. Logical NOT assignment operator (!=)
+        System.out.println(true != false);
+        
+        // 8. Bitwise AND assignment operator (&=)
+        int k = 2;
+        k &= 2;
+        System.out.println(k);
+        
+        // 9. Bitwise OR assignment operator (|=)
+        int l = 2;
+        l |= 2;
+        System.out.println(l);
+        
+        // 10. Bitwise XOR assignment operator (^=)
+        int m = 2;
+        m ^= 2;
+        System.out.println(m);
+        
+        // 11. Left shift assignment operator (<<=)
+        int n = 2;
+        n <<= 2;
+        System.out.println(n);
+        
+        // 12. Right shift assignment operator (>>=)
+        int o = 2;
+        o >>= 2;
+        System.out.println(o);
+        
+        //13. Unsigned right shift assignment operator (>>>=)
+        int p = 2;
+        o >>>= 2;
+        System.out.println(p);
+    }
+}
