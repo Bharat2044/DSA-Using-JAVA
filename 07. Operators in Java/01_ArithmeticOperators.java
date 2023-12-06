e8645a35ac11ea9ceae5cf7a220ac2ed9e17180f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 02:16:43 +0530

    code added

---
+++ 07. Operators in Java/01_ArithmeticOperators.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(a % 2.2);
+    }
+}
