8f8eb36d6c274cafa82a397714f4e0435b5e78cf
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 02:28:35 +0530

    code added

---
+++ 07. Operators in Java/03_RelationalOperator.java
@@ -0,0 +1,26 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 7, b = 11;
+    
+        // value of a and b
+        System.out.println("a is " + a + " and b is " + b);
+    
+        // == operator
+        System.out.println(a == b); 
+    
+        // != operator
+        System.out.println(a != b);  
+    
+        // > operator
+        System.out.println(a > b);
+    
+        // < operator
+        System.out.println(a < b);  
+    
+        // >= operator
+        System.out.println(a >= b);  
+    
+        // <= operator
+        System.out.println(a <= b); 
+    }
+}
