4397336776517a0e96d11f7d450f99ad231d285f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-06 23:58:58 +0530

    code added

---
+++ 03. Datatype in Java/02_Non-Primitive Datatype/01_StringDatatype.java
@@ -0,0 +1,21 @@
+public class StringDatatype {
+        
+    public static void main(String args[]) {
+        String a = "Bharat";
+        String b = "Raju Kumar Raj";
+        String c = "0";
+        String d = "12345";
+        
+        String e = "10.5";
+        String f = "-129878";
+        String g = "abc123!2@";
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+    }
+}
