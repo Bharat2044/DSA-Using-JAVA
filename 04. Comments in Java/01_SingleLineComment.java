abb88ded9879361115856463d5cbe004b0969072
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 00:00:56 +0530

    code added

---
+++ 04. Comments in Java/01_SingleLineComment.java
@@ -0,0 +1,8 @@
+public class SingleLineComment {
+        
+    public static void main(String args[]) {        
+        
+        // This is sinlge line comment
+        System.out.println("Single Line Comment");
+    }
+}
