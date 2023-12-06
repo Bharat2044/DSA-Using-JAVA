25a973762794ffe69c0ee00ebdceea5dba9b64c8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 03:00:26 +0530

    code added

---
+++ 08. Basic Problems/02_PrintABCDE.java
@@ -0,0 +1,26 @@
+/*
+Problem Description:
+Print the first five letters of the English alphabet i.e. A, B, C, D and E.
+
+Output Format:
+Print the characters in separate lines.
+
+Example Output:
+A
+B
+C
+D
+E
+*/
+
+
+public class PrintABCDE {
+    public static void main(String[] args) {
+ 
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+    }
+}
