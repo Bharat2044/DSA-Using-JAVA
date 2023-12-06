c599ed4ae948d7648ad6e748bdac0fe274c39ae1
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 00:14:19 +0530

    code added

---
+++ 05. Input-Output in Java/10_PrintName.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PrinName {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        
+        System.out.println("Your name is: " + name);
+    }
+}
