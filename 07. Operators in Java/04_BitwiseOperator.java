26abb9d7022d1173193a66e898bef5644e1eb44d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 02:35:31 +0530

    code added

---
+++ 07. Operators in Java/04_BitwiseOperator.java
@@ -0,0 +1,14 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(a >> b);
+        System.out.println(a >>> b);
+        System.out.println(a << b);
+        System.out.println(~a);
+    }
+}
