503b86d7cc9129d0650b23e1e8bcddc1da3736bc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 03:48:06 +0530

    code added

---
+++ 08. Basic Problems/08_AverageOfFiveNumbers.java
@@ -0,0 +1,39 @@
+/*
+Problem Description:
+Given 5 numbers A, B, C, D, E as input. Print the average of these 5 numbers (correct up to 2 decimal places).</div>
+
+Problem Constraints:
+1 <= A, B, C, D, E <= 104</div>
+
+Input Format:
+There are five lines in the input.
+Each line has an integer.</div>
+
+Output Format:
+Print the average of the input numbers (correct up to 2 decimal places).</div>
+
+Example Input:
+Input:-
+2 2 51 27 73
+
+Example Output:
+Output:-
+31.00
+*/
+
+
+import java.util.Scanner;
+public class AverageOfFiveNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        
+        double avg = (double)((a + b + c + d + e) / 5);
+        
+        System.out.println();
+    }
+}
