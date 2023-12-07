7798b97c45d20f9973908ff987921244f196d13e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-07 21:48:37 +0530

    code added

--- 11. Loops in Java/EvenGame.java
+++ 11. Loops in Java/EvenGame.java
@@ -27,7 +27,7 @@ Output 2:
 
 import java.util.Scanner;
 
-class HelloWorld {
+class EvenGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
