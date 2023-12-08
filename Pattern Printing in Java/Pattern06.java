45bc2acb4dfbdd5b85a533ba6951bb5bef6307f3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 23:33:09 +0530

    hollow half pyramid pattern program code added

--- Pattern Printing in Java/Pattern06.java
+++ Pattern Printing in Java/Pattern06.java
@@ -10,7 +10,7 @@
 
 import java.util.Scanner;
 
-public class Pattern04 {
+public class Pattern06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
