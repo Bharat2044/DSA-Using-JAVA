c56c4669483b6551adb9d7f8f4ff908f6207a4c0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 23:52:31 +0530

    hollow inverted full pyramid pattern program code added

---
+++ Pattern Printing in Java/Pattern16.java
@@ -0,0 +1,33 @@
+/*
+ n = 5
+* * * * *
+ *     *
+  *   *
+   * *
+    *
+*/
+
+
+
+import java.util.Scanner;
+
+public class Pattern16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++) {
+            for(int s=1; s<=i-1; s++)
+                System.out.print(" ");
+                
+            for(int j=1; j<=n-i+1; j++) {
+                if((i==1) || (j==1) || (j==n-i+1))
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
