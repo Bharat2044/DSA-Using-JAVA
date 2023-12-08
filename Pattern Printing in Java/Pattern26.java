c210e2346c4cd69115d901ef185361164c73cd40
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-09 02:25:17 +0530

    number hollow inverted half pyramid pattern code added

---
+++ Pattern Printing in Java/Pattern26.java
@@ -0,0 +1,29 @@
+/*
+ n = 5
+1 2 3 4 5
+1     4
+1   3 
+1 2
+1
+*/
+
+
+import java.util.Scanner;
+
+public class Pattern25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++) {
+                if(i==1 || j==1 || j==n-i+1)
+                    System.out.print(j + " ");
+                else
+                    System.out.print("  ");
+            }
+                
+            System.out.println();
+        }
+    }
+}
