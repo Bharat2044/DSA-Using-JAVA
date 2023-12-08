a8a5d292f32106605e5b57c1558008dabcacc6e9
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 23:27:39 +0530

    hollow rectangle pattern program code added

---
+++ Pattern Printing in Java/Pattern04.java
@@ -0,0 +1,27 @@
+/*
+    m = 3, n = 5
+    * * * * *
+    *       *
+    * * * * *
+*/
+
+import java.util.Scanner;
+
+public class Pattern04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if((i==1) || (i==m) || (j==1)|| (j==n))
+                    System.out.print("* ");
+                else 
+                    System.out.print("  ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
