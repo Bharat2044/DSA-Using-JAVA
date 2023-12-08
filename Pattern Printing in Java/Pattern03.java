c0187d79e552d865a71fd1fe37e22a2a0212a9d0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 23:24:11 +0530

    solid rectangle pattern program code

---
+++ Pattern Printing in Java/Pattern03.java
@@ -0,0 +1,23 @@
+/*
+    m = 3, n = 5
+    * * * * *
+    * * * * *
+    * * * * *
+*/
+
+import java.util.Scanner;
+
+public class Pattern03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++)
+                System.out.print("* ");
+                
+            System.out.println();
+        }
+    }
+}
