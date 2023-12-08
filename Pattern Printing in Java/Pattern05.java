190e11cd82caf0bdfb1034231122bd720d8ffb77
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 23:33:32 +0530

    half pyramid pattern program code added

--- Pattern Printing in Java/Pattern05.java
+++ Pattern Printing in Java/Pattern05.java
@@ -7,9 +7,11 @@
     * * * * *
 */
 
+
+
 import java.util.Scanner;
 
-public class Pattern04 {
+public class Pattern05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
