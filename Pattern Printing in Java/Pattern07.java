8d4e95d17031bda7c58ebb3f2ab7653a2a7f501e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 23:32:41 +0530

    inverted half pyramid pattern program code added

---
+++ Pattern Printing in Java/Pattern07.java
@@ -0,0 +1,25 @@
+/*
+    n = 5
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
+*/
+
+
+import java.util.Scanner;
+
+public class Pattern07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++)
+                System.out.print("* ");
+            
+            System.out.println();
+        }
+    }
+}
