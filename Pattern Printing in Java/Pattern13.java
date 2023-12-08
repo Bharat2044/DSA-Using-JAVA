2b947bcc9dca0290114ba71fbe9bb10befd4519e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-08 23:47:10 +0530

    full pyramid pattern program code added

---
+++ Pattern Printing in Java/Pattern13.java
@@ -0,0 +1,29 @@
+/*
+    n = 5
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+*/
+
+
+
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++) {
+            for(int s=1; s<=n-i; s++)
+                System.out.print(" ");
+                
+            for(int j=1; j<=i; j++)
+                System.out.print("* ");
+            
+            System.out.println();
+        }
+    }
+}
