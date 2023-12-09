39e05d4c6a48b708828c9b6208476bffa04f2a83
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-09 08:47:54 +0530

    alphabet half pyramid pattern program code added

---
+++ Pattern Printing in Java/Pattern50.java
@@ -0,0 +1,26 @@
+/*
+n = 5
+A 
+A B 
+A B C 
+A B C D 
+A B C D E
+*/
+
+
+
+import java.util.Scanner;
+
+public class Pattern50 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++) {
+            for(char j=1; j<=i; j++)
+                System.out.print((char)('A'+j-1) + " ");
+                
+            System.out.println();
+        }
+    }
+}
