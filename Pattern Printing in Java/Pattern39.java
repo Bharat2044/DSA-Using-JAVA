93a82922daac9f47c005b866df09158e52a191cc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-09 08:30:39 +0530

    fancy floyd's triangle pattern program code added

--- Pattern Printing in Java/Pattern39.java
+++ Pattern Printing in Java/Pattern39.java
@@ -18,7 +18,6 @@ public class Pattern39 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 1;
         
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=i; j++) {
