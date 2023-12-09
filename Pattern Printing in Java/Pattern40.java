f1c4a9f081cb8d8be0a8c2cce3bdbd71379d1c8f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-09 08:30:08 +0530

    pascal traingle pattern program code added

--- Pattern Printing in Java/Pattern40.java
+++ Pattern Printing in Java/Pattern40.java
@@ -26,7 +26,6 @@ public class Pattern40 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 1;
         
         // 1st Approach => TC = O(n^3), SC = O(1)
         for(int i=0; i<n; i++) {
