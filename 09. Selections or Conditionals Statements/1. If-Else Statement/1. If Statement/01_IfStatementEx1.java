d25cfeea4ca3a5e682a661d1d6e288abef0ed343
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 07:04:05 +0000

    code added

---
+++ 09. Selections or Conditionals Statements/1. If-Else Statement/1. If Statement/01_IfStatementEx1.java
@@ -0,0 +1,10 @@
+public class IfStatementEx1 {
+    public static void main(String[] args) {
+        if(true)
+            System.out.println("Hello");
+          
+        if(false)
+            System.out.println("Hi");
+      
+    }
+}
