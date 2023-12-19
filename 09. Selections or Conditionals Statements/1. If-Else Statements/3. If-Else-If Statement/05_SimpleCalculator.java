506ee6d99c3012317579baac88772171013f902c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 06:02:25 +0000

    code added

--- 09. Selections or Conditionals Statements/1. If-Else Statements/3. If-Else-If Statement/05_SimpleCalculator.java
+++ 09. Selections or Conditionals Statements/1. If-Else Statements/3. If-Else-If Statement/05_SimpleCalculator.java
@@ -4,7 +4,7 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter an operator (+, -, *, /): ");
+        System.out.print("Enter an operator (+, -, *, /, %): ");
         char oper = sc.next().charAt(0);
         
         System.out.println("Enter two numbers: ");
@@ -21,7 +21,7 @@ public class SimpleCalculator {
             System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
         else if(oper == '%')
             System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
-        else    // operator is doesn't match any case constant (+, -, *, /)
+        else    // operator is doesn't match any case constant (+, -, *, /, %)
             System.out.println("Error! You entered a wrong operator, please enter a correct operator");
     }
 }
