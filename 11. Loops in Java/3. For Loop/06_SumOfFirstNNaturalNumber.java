adce8e46f334ce6c09dec39e5e614a453ce3b284
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 10:53:56 +0000

    code added

---
+++ 11. Loops in Java/3. For Loop/06_SumOfFirstNNaturalNumber.java
@@ -0,0 +1,29 @@
+// C++ program to find the sum of first n natural numbers
+// positive integers such as 1,2,3,...n are known as natural numbers
+
+
+import java.util.Scanner;
+
+public class SumOfFirstNNaturalNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int sum = 0;
+        
+         // Approach - 1 => Time Complexity = O(n)
+        /*
+        for (int i=1; i<=num; i++) {
+            sum += i;
+        }
+        */
+        
+        
+        // Approach - 2 => Time Complexity = O(1)
+        sum = (num*(num+1) / 2);
+    
+        System.out.println("Sum = " + sum);
+    }
+}
