06bf2ce76c9ca9050bbe942e5fd24519d9925a5e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 04:16:35 +0000

    code added

---
+++ 09. Selections or Conditionals Statements/1. If-Else Statements/3. If-Else-If Statement/02_IfElseIfStatementEx2.java
@@ -0,0 +1,14 @@
+public class IfElseIfStatementEx2 {
+    public static void main(String[] args) {
+        int n = 20;
+
+		if (n == 10)
+			System.out.println("i is 10");
+		else if (n == 15)
+			System.out.println("n is 15");
+		else if (n == 20)
+			System.out.println("n is 20");
+		else
+			System.out.println("n is not present");
+    }
+}
