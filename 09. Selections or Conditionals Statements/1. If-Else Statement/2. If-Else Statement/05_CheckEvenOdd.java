2dcc6cdd93fe0c6e0f396702efd0b8ad913c2408
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 07:05:15 +0000

    code added

---
+++ 09. Selections or Conditionals Statements/1. If-Else Statement/2. If-Else Statement/05_CheckEvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CheckEvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+    
+        if(num % 2 == 0) {
+            System.out.println(num + " is an even number");
+        }
+        else {
+            System.out.println(num + " is an odd number");
+        }
+    }
+}
