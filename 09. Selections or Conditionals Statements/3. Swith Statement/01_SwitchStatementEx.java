a74b785492556bf5191a0ec6bea72d04a6969bca
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 07:10:21 +0000

    code added

---
+++ 09. Selections or Conditionals Statements/3. Swith Statement/01_SwitchStatementEx.java
@@ -0,0 +1,26 @@
+public class SwitchStatementEx {
+    public static void main(String[] args) {
+        int num = 3;
+    
+        switch(num) {
+            case 1:
+                System.out.println("Number is " + num);
+                break;
+                
+            case 2:
+                System.out.println("Number is " + num);
+                break;
+                
+            case 3:
+                System.out.println("Number is " + num);
+                break;
+                
+            case 4:
+                System.out.println("Number is " + num);
+                break;
+                
+            default:
+                System.out.println("Default Case");
+        }
+    }
+}
