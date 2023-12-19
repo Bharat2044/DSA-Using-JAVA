782e20b8ed7bcbe8bd39b863b7d3d88564a625a5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 09:49:39 +0000

    code added

---
+++ 09. Selections or Conditionals Statements/5. Selection Statement Programs/28_SimpleCalculatorUsingSwitchExpression.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SimpleCalculatorUsingSwitchExpression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter an operator (+, -, *, /, %) ->  ");
+        char oper = sc.next().charAt(0);
+        
+        System.out.println("Enter two numbers ->  ");
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+
+        switch(oper) {
+            case '+' -> System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                
+            case '-' -> System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                
+            case '*' -> System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                
+            case '/' -> System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                
+            case '%' -> System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+                            
+            default -> System.out.println("Error! You entered a wrong operator, please enter a correct operator");                
+        }
+    }
+}
