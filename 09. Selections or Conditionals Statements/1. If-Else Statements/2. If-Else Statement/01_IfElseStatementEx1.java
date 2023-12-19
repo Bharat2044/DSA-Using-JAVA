af29cc7773fbd08a701e56ec75f0ea2f7ee2975e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 04:15:39 +0000

    code added

---
+++ 09. Selections or Conditionals Statements/1. If-Else Statements/2. If-Else Statement/01_IfElseStatementEx1.java
@@ -0,0 +1,15 @@
+public class IfElseStatementEx1 {
+    public static void main(String[] args) {
+        if(true)
+            System.out.println("Hi");
+        else
+            System.out.println("Hello");
+            
+            
+        if(false)
+            System.out.println("Bharat");
+        else
+            System.out.println("Deepak");
+      
+    }
+}
