0e195994ec26c9d71566172f88bb0c2e4fab569b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 07:05:07 +0000

    code added

---
+++ 09. Selections or Conditionals Statements/1. If-Else Statement/2. If-Else Statement/02_IfElseStatementEx2.java
@@ -0,0 +1,12 @@
+public class IfElseStatementEx2 {
+    public static void main(String[] args) {
+        int n = 10;
+  
+      	if (n > 15) {
+      		  System.out.println(n + " is greater than 15");
+      	}
+		else {
+      		System.out.println(n + " is less than 15");
+		}
+    }
+}
