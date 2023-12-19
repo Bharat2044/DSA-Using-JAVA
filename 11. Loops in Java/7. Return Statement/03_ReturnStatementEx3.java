eb7ce9f2b59b18c4ae9d994242ce33467bd6822b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-19 11:36:57 +0000

    code added

---
+++ 11. Loops in Java/7. Return Statement/03_ReturnStatementEx3.java
@@ -0,0 +1,14 @@
+public class ReturnStatementEx2 {
+  
+    public static void print() {
+        System.out.println("Hello");
+        return;
+    }
+  
+    public static void main(String[] args) {
+        // Calling print
+	    print();
+	      
+	    return;
+    }
+}
