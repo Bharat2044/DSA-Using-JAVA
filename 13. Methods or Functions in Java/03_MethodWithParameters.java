6c417c8045bef571751c05fb4e7dfd9f41622751
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-24 14:02:42 +0000

    code added

---
+++ 13. Methods or Functions in Java/03_MethodWithParameters.java
@@ -0,0 +1,11 @@
+public class MethodWithParameters { 
+
+    public static void greet(String name) {
+        System.out.println("Hi " + name);
+    }
+    
+    public static void main(String[] args) {
+        greet("Rahul");
+        greet("Bharat");
+    }
+}
