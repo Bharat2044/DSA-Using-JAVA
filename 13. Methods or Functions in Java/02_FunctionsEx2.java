bded281628bca9bb6a1e89da6dce29acc8adaa73
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-24 13:49:14 +0000

    code added

---
+++ 13. Methods or Functions in Java/02_FunctionsEx2.java
@@ -0,0 +1,15 @@
+public class FunctionsEx2 { 
+
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello");
+    }
+    
+    public static void main(String[] args) {
+        sayHi();
+        sayHello();
+    }
+}
