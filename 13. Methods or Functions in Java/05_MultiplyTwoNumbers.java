1f843d1710aa7fdbeb8e430d82dc795f91b8af4e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-24 14:02:55 +0000

    code added

---
+++ 13. Methods or Functions in Java/05_MultiplyTwoNumbers.java
@@ -0,0 +1,13 @@
+public class MultiplyTwoNumbers { 
+
+    public static void product(int a, int b) {
+        System.out.println(a * b);
+    }
+    
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        
+        product(a, b);
+    }
+}
