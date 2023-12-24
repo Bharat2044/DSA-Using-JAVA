87a6971c480afaef7ef8bbbd9418dc32ea7f4b68
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-24 17:48:54 +0000

    code added

--- 13. Methods or Functions in Java/33_AreaOfCircle.java
+++ 13. Methods or Functions in Java/33_AreaOfCircle.java
@@ -1,7 +1,7 @@
 /*
 Problem Description:
 You are given a positive integer A denoting the radius of a circle. You have to calculate the area of the circle.
-Arae of a circle having radius R is given by (π * R2).
+Arae of a circle having radius R is given by (π * r^2).
 
 NOTE: Since, the answer can be a real number, you have to return the ceil value of the area. Ceil value of a real number X is the smallest integer that is greater than or equal to X. Use standard PI value in the particular programming language.
 
