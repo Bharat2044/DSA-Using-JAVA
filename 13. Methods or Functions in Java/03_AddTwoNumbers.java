fc2dc5fd392587e7b2456f67a9cb1e9933bac5e8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-24 13:49:18 +0000

    code added

---
+++ 13. Methods or Functions in Java/03_AddTwoNumbers.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers { 
+
+    public static void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        sum(a, b);
+    }
+}
