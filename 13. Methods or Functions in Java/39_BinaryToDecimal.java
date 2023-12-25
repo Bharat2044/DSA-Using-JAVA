697d0b4fa7cd625311130a74ffe2cef3b6a97153
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-25 15:20:05 +0530

    code added

--- 13. Methods or Functions in Java/39_BinaryToDecimal.java
+++ 13. Methods or Functions in Java/39_BinaryToDecimal.java
@@ -31,39 +31,6 @@ Explanation 2:
 */
 
 
-/*
-Problem Description:
-Take a number A as input in binary format (Base = 2). You have to print the number in decimal format (Base = 10).
-
-Problem Constraints:
-1 <= A <= 210
-
-Input Format:
-First and only line contains a single binary number A.
-
-Output Format:
-Print in a single line, a decimal integer.
-
-Example Input:
-Input 1:
-11 
-Input 2:
-1011 
-
-Example Output:
-Output 1:
-3 
-Output 2:
-11 
-
-Example Explanation:
-Explanation 1:
-(0011)2 = (21 + 20)10 = (3)10 
-Explanation 2:
-(1011)2 = (23+ 21 + 20)10 = (11)10 
-*/
-
-
 import java.util.Scanner;
 
 public class BinaryToDecimal { 
