c16c227b46d942a0247c723a386ae7e6c8e29519
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-27 16:37:35 +0530

    Create 30_PerimeterOfParallelogram.java

---
+++ 08. Basic Problems/30_PerimeterOfParallelogram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PerimeterOfParallelogram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base and side of parallelogram: ");
+        double b = sc.nextDouble();
+        double a = sc.nextDouble();
+
+        double perimeter = 2 * (a * b);
+        
+        System.out.println("Perimeter Of Parallelogram = " + perimeter);
+        
+        sc.close();
+    }
+}
