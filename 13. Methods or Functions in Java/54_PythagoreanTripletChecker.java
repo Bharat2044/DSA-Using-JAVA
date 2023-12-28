26c1113cd0acfb523973a370e132fa344d250c02
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-28 13:22:52 +0530

    code added

---
+++ 13. Methods or Functions in Java/54_PythagoreanTripletChecker.java
@@ -0,0 +1,35 @@
+// Write a function to check if a given triplet is a Pythagorean triplet or not. 
+// (A Pythagorean triplet is when the sum of the square of two numbers is equal to the square of the third number).
+
+
+import java.util.Scanner;
+
+public class PythagoreanTripletChecker {
+  
+    public static boolean isPythagoreanTriplet(int a, int b, int c) {
+        int min = Math.min(Math.min(a, b), c);
+        int max = Math.max(Math.max(a, b), c);
+        int middle = a + b + c - min - max;
+
+        // Check the Pythagorean triplet condition
+        return ((min * min + middle * middle) == (max * max));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 3 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if (isPythagoreanTriplet(a, b, c)) {
+            System.out.println("The triplet (" + a + ", " + b + ", " + c + ") is a Pythagorean triplet.");
+        } 
+        else {
+            System.out.println("The triplet (" + a + ", " + b + ", " + c + ") is not a Pythagorean triplet.");
+        }
+            
+        sc.close();
+    }
+}
