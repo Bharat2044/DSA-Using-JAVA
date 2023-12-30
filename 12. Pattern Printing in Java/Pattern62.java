b8362101b2efcb3a4f61d26fc448755edc307361
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-30 22:04:32 +0530

    Z pattern code added

---
+++ 12. Pattern Printing in Java/Pattern62.java
@@ -0,0 +1,32 @@
+/*
+    n = 5
+    *****
+      *  
+     *   
+    *    
+    *****
+*/
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(i == 1 || i == n || j == (n-i)) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.println();
+        }
+    }
+}
