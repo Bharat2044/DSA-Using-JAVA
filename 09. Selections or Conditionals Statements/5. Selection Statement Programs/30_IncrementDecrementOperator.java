fd377dc0485ca6e6ea170e6efb50715fdc1b79b3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-30 22:26:12 +0530

    code added

---
+++ 09. Selections or Conditionals Statements/5. Selection Statement Programs/30_IncrementDecrementOperator.java
@@ -0,0 +1,17 @@
+public class IncrementDecrementOperator {
+
+    public static void main(String[] args) {
+        
+        int i = 10;
+        if(i++ == i)
+     	      System.out.println(i + " is good");
+        else
+     	      System.out.println(i + " is bad");
+     
+        int j = 20;
+        if(++j == j)
+     	      System.out.println(j + " is good");
+        else
+     	      System.out.println(j + " is bad");
+    }
+}
