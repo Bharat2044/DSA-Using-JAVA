0e63a1805748d4f68d06754c65b5b72854046581
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:21:08 +0000

    code added

---
+++ 10. Math Class in Java/2. Logarithmic Math Methods/01_LogValue.java
@@ -0,0 +1,21 @@
+public class LogValue {
+    public static void main(String[] args) {
+
+        System.out.println(Math.log(5)); 
+        System.out.println(Math.log(-5)); 
+        System.out.println(Math.log(0)); 
+        
+        System.out.println(Math.log(68)); 
+        System.out.println(Math.log(100)); 
+        
+        System.out.println(Math.log(38.9)); 
+        System.out.println(Math.log(-70.4)); 
+        
+        System.out.println(Math.log(1.0/0)); 
+        System.out.println(Math.log(0.0/0)); 
+        
+        System.out.println((int)Math.log(10)); 
+        System.out.println((int)Math.log(85)); 
+        System.out.println((int)Math.log(110)); 
+    }
+}
