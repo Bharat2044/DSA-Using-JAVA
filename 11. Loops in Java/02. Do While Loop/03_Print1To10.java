58d9188516383a24af34370aa7609e09deaed34b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:29:55 +0000

    code added

---
+++ 11. Loops in Java/02. Do While Loop/03_Print1To10.java
@@ -0,0 +1,10 @@
+public class Print1To10 {
+    public static void main(String[] args) {
+        int i = 1; 
+    
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 10);
+    }
+}
