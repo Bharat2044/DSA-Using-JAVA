20cb86105a5f42b1f253b1c84ac32e5f2c5c337e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:31:26 +0000

    code added

---
+++ 11. Loops in Java/05. Break Statement/01_BreakStatementEx1.java
@@ -0,0 +1,11 @@
+public class BreakStatementEx1 {
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=10; i++) {
+            if(i > 5)
+                break;
+            
+            System.out.println("Hello");
+        } 
+    }
+}
