48f0f8d1b015fc2a50f43f1b6cc25b63f360376a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:08:15 +0000

    code added

---
+++ 10. Math Class in Java/1. Basic Math Methods/09_SquareRootValue.java
@@ -0,0 +1,15 @@
+public class SquareRootValue {
+    public static void main(String[] args) {
+      
+        System.out.println(Math.sqrt(5));  
+        System.out.println(Math.sqrt(81));  
+        System.out.println(Math.sqrt(-5));  
+        
+        System.out.println(Math.sqrt(0.0));  
+        System.out.println(Math.sqrt(4.5));  
+        System.out.println(Math.sqrt(16.9f));  
+        
+        System.out.println(Math.sqrt(1.0/0));  
+        System.out.println(Math.sqrt(0.0/0));  
+    }
+}
