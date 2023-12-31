1cd62c0ec10ab8a552fa7aca9a9fb452823b5d39
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:31:45 +0000

    code added

---
+++ 11. Loops in Java/06. Continue Statement/01_ContinueStatementEx1.java
@@ -0,0 +1,11 @@
+public class ContinueStatementEx1 {
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=10; i++) {
+            if(i > 5)
+                continue;
+            
+            System.out.println("Hello");
+        } 
+    }
+}
