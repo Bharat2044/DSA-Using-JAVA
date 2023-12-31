6378f181083e9afb2bb2d5cbab69f82dfecde16a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:26:27 +0000

    code added

---
+++ 11. Loops in Java/01. While Loop/01_WhileLoopEx1.java
@@ -0,0 +1,10 @@
+public class WhileLoopEx1 {
+    public static void main(String[] args) {
+        int i = 1; 
+    
+        while (i <= 5) {
+            System.out.println("Hello");
+            ++i;
+        }
+    }
+}
