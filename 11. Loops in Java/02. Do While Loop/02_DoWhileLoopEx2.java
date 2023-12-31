e41cf1f8f65c85f57c3f35691dda81630454b069
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:29:50 +0000

    code added

---
+++ 11. Loops in Java/02. Do While Loop/02_DoWhileLoopEx2.java
@@ -0,0 +1,10 @@
+public class DoWhileLoopEx2 {
+    public static void main(String[] args) {
+        int i = 5; 
+    
+        do {
+            System.out.println("Hello");
+            i--;
+        } while (i >= 1);
+    }
+}
