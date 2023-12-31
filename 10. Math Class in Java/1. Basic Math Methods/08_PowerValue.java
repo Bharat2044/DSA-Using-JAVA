f5901ca02ab83022ab70a121febdfc7a96900c95
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:08:12 +0000

    code added

---
+++ 10. Math Class in Java/1. Basic Math Methods/08_PowerValue.java
@@ -0,0 +1,13 @@
+public class PowerValue {
+    public static void main(String[] args) {
+      
+        System.out.println(Math.pow(5, 3));   
+        System.out.println(Math.pow(-5, 3));   
+        System.out.println(Math.pow(5, -3));   
+        System.out.println(Math.pow(-5, -3));   
+        
+        System.out.println(Math.pow(2.5, 4));   
+        System.out.println(Math.pow(9.0, -2));   
+        System.out.println(Math.pow(-765, 0.2));   
+    }
+}
