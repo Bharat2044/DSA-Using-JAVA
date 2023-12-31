083b6b916dcd130b28848f7e38b4ee42c7ca5638
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:08:08 +0000

    code added

---
+++ 10. Math Class in Java/1. Basic Math Methods/07_MinimumValue.java
@@ -0,0 +1,12 @@
+public class MinimumValue {
+    public static void main(String[] args) {
+      
+        System.out.println(Math.min(20, 50));   
+        System.out.println(Math.min(345L, 3458L));
+        
+        System.out.println(Math.min(-25.74f, -20.38f));
+        System.out.println(Math.min(-25.67, 87.232));
+        
+        System.out.println(Math.min(1, Math.min(5, -3)));
+    }
+}
