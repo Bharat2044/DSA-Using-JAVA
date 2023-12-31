aa1aca6069ed97b4ffcca0089f2abfcf957f6944
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-31 14:08:04 +0000

    code added

---
+++ 10. Math Class in Java/1. Basic Math Methods/06_MaximumValue.java
@@ -0,0 +1,12 @@
+public class MaximumValue {
+    public static void main(String[] args) {
+      
+        System.out.println(Math.max(20, 50));   
+        System.out.println(Math.max(345L, 3458L));
+        
+        System.out.println(Math.max(-25.74f, -20.38f));
+        System.out.println(Math.max(-25.67, 87.232));
+        
+        System.out.println(Math.max(1, Math.max(5, -3)));
+    }
+}
