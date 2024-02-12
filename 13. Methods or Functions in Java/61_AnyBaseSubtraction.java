6156388c6ca426484aff51dfa62e2c0968958c28
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-02-12 18:06:25 +0530

    code added

---
+++ 13. Methods or Functions in Java/61_AnyBaseSubtraction.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class AnyBaseSubtraction {
+    
+    public static int getDifference(int n1, int n2, int b) {
+        int ans = 0, pow = 1, borrow = 0;
+        
+        while(n1 > 0) {
+            int d1 = n1 % 10; // d1 = (n1 > 0) ? (n1 % 10) : 0;
+            int d2 = n2 % 10; // d2 = (n2 > 0) ? (n2 % 10) : 0;
+            
+            n1 /= 10;
+            n2 /= 10;
+            
+            int sub = borrow + d1 - d2;
+            
+            if(sub < 0) {
+              sub += 10;
+              borrow = -1;
+            }
+            else  {
+              borrow = 0;
+            }
+            
+            ans += sub * pow;
+            pow *= 10;
+        }
+        
+        return ans;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int base = sc.nextInt();
+        
+        int result = getDifference(n1, n2, base);
+        
+        System.out.println(result);
+    }
+}
