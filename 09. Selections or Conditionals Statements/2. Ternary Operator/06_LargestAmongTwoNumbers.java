1d43ce736549147d41543aea89e511929ac07f5c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-12 12:45:55 +0530

    code added

--- 09. Selections or Conditionals Statements/2. Ternary Operator/06_LargestAmongTwoNumbers.java
+++ 09. Selections or Conditionals Statements/2. Ternary Operator/06_LargestAmongTwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LargestAmongThreeNumbers {
+public class LargestAmongTwoNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
