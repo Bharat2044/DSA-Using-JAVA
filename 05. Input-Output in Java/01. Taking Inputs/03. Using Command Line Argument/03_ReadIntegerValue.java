673bb87e25371019b4fab6d67cf282f3d09845f5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:21 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/03_ReadIntegerValue.java
@@ -0,0 +1,8 @@
+public class _03_ReadIntegerValue {
+    public static void main(String args[]) {
+        
+        int a = Integer.parseInt(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
