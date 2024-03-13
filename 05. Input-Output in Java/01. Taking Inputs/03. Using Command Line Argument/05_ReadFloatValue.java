30464a6e183d56bcd7ebfeb437bcf122fcc1ed73
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:30 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/05_ReadFloatValue.java
@@ -0,0 +1,8 @@
+public class _05_ReadFloatValue {
+    public static void main(String args[]) {
+        
+        float a = Float.parseFloat(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
