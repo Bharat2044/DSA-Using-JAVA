6d870c26fd2cf0a4eb28655bbbdc20de737370bd
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:34 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/06_ReadDoubleValue.java
@@ -0,0 +1,8 @@
+public class _06_ReadDoubleValue {
+    public static void main(String args[]) {
+        
+        double a = Double.parseDouble(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
