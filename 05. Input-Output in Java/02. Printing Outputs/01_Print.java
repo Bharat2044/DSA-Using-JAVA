f354f17e0758d6698f2615f8789a9aecb9b10c98
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:34:05 +0000

    code added

---
+++ 05. Input-Output in Java/02. Printing Outputs/01_Print.java
@@ -0,0 +1,48 @@
+public class Print {
+
+	public static void main(String[] args) {
+
+		String name = "Bharat";
+		byte age = 21;
+		short dob = 2003;
+		int price = 209761;
+		long mobileNumber = 9977886655L;
+		float marks = 99.99f;
+		double bankBalance = 23448599.984;
+		char section = 'A';
+		boolean isStudent = true;
+
+		System.out.print(name);
+		System.out.print(age);
+		System.out.print(dob);
+		System.out.print(price);
+		System.out.print(mobileNumber);
+		System.out.print(marks);
+		System.out.print(bankBalance);
+		System.out.print(section);
+		System.out.print(isStudent);
+
+		System.out.println();
+
+		System.out.print(name + "\n");
+		System.out.print(age + "\n");
+		System.out.print(dob + "\n");
+		System.out.print(price + "\n");
+		System.out.print(mobileNumber + "\n");
+		System.out.print(marks + "\n");
+		System.out.print(bankBalance + "\n");
+		System.out.print(section + "\n");
+		System.out.print(isStudent + "\n");
+
+		System.out.print(name + "  ");
+		System.out.print(age + "  ");
+		System.out.print(dob + "  ");
+		System.out.print(price + "  ");
+		System.out.print(mobileNumber + "  ");
+		System.out.print(marks + "  ");
+		System.out.print(bankBalance + "  ");
+		System.out.print(section + "  ");
+		System.out.print(isStudent + "  ");
+
+	}
+}
