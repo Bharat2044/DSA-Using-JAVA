036e35106db9501022d8103db7eba2fcb8658c64
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:25 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/04_ReadLongValue.java
@@ -0,0 +1,8 @@
+public class _04_ReadLongValue {
+    public static void main(String args[]) {
+        
+        long a = Long.parseLong(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
