00400f7fc4352adb9000cb490dc01c87565d2338
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:16 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/02_ReadShortValue.java
@@ -0,0 +1,8 @@
+public class _02_ReadShortValue {
+    public static void main(String args[]) {
+        
+        short a = Short.parseShort(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
