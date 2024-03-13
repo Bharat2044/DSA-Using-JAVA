a4824b6a2e3c37a65f1ac1c380921ff06fa0311b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:54 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/11_SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+public class _11_SumOfTwoNumbers {
+	public static void main(String args[]) {
+        
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        
+        int sum = a + b;
+        
+        System.out.println("Sum = " + a);
+    }
+}
