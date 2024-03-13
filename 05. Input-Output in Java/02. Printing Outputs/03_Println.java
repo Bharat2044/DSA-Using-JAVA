dfc1b7894021d3b9cbdf99cda94abd5a9ee92a3b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:34:11 +0000

    code added

---
+++ 05. Input-Output in Java/02. Printing Outputs/03_Println.java
@@ -0,0 +1,25 @@
+public class Println {
+
+	public static void main(String[] args) {
+
+		String name = "Bharat";
+		byte age = 21;
+		short dob = 2003;
+		int price = 209761;
+		long mobileNumber = 9977886655L;
+		float marks = 99.99f;
+		double bankBalance = 23448599.984;
+		char section = 'A';
+		boolean isStudent = true;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(dob);
+		System.out.println(price);
+		System.out.println(mobileNumber);
+		System.out.println(marks);
+		System.out.println(bankBalance);
+		System.out.println(section);
+		System.out.println(isStudent);
+	}
+}
