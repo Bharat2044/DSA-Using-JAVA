9e7fcae43675861a3d81980da7548c23b1d8bbb1
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:32:57 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/02. Using BufferedReader/10_PrintName.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10_PrintName {
+	public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Enter Your Name: ");
+        String name = br.readLine();
+        
+        System.out.println("Yoyr name is: " + name);
+        
+    }
+}
