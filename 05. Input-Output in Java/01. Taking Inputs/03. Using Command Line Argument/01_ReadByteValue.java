12597031513445a79aacd45f6d0d5bb056160e92
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:12 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/01_ReadByteValue.java
@@ -0,0 +1,8 @@
+public class _01_ReadByteValue {
+    public static void main(String args[]) {
+        
+        byte a = Byte.parseByte(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
