ed5a5162ca2218fcf72ce52af0281171b6ecf34d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:59 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/12_SumOfThreeNumbers.java
@@ -0,0 +1,12 @@
+public class _12_SumOfThreeNumbers {
+	public static void main(String args[]) {
+
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		int sum = a + b + c;
+
+		System.out.println("Sum = " + a);
+	}
+}
