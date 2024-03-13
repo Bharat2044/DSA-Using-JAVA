206b1b015ec48672d98027e200a841f08ffe6bcd
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 12:34:26 +0530

    code added

---
+++ 03. Datatype in Java/01_Prmitive Datatype/08_BooleanDatatype.java
@@ -0,0 +1,15 @@
+public class _07_BooleanDatatype {
+    static boolean x;
+    
+    public static void main(String args[]) {
+    	System.out.println("Default value of boolean = " + x);
+    	
+        boolean a = true;     
+        boolean b = false;    
+        
+        // boolean c = 0;  // BooleanDataType.java:12: error: incompatible types: int cannot be converted to boolean
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
