7fb7bdcd9891c72b4a1d29a39ef438b0ab90127b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:38 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/07_ReadBooleanValue.java
@@ -0,0 +1,8 @@
+public class _07_ReadBooleanValue {
+    public static void main(String args[]) {
+        
+        boolean a = Boolean.parseBoolean(args[0]);
+        
+        System.out.println("Result: " + a);
+    }
+}
