76bd747835ca758123ef5b810da2adbf9d727c70
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-03-13 07:33:42 +0000

    code added

---
+++ 05. Input-Output in Java/01. Taking Inputs/03. Using Command Line Argument/08_ReadCharacterValue.java
@@ -0,0 +1,8 @@
+public class _08_ReadCharacterValue {
+    public static void main(String args[]) {
+        
+        char a = args[0].charAt(0);
+        
+        System.out.println("Result: " + a);
+    }
+}
