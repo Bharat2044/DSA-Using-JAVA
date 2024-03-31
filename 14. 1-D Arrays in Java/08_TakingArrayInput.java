8b73766e2b1691307f202792ed5d30e25f3704b8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-01 04:11:20 +0530

    code added

---
+++ 14. 1-D Arrays in Java/08_TakingArrayInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TakingArrayInput {
+  	public static void main(String[] args) {
+  		
+    		Scanner sc = new Scanner(System.in);
+    		
+    		System.out.print("Enter size of the array: ");
+    		int n = sc.nextInt();
+    		
+    		int[] arr = new int[n];
+    		
+    		System.out.println("Enter array elements: ");
+    		for(int i = 0; i < n; i++) {
+    			arr[i] = sc.nextInt();
+    		}
+    		
+    		System.out.print("Array elments are: ");
+    		for(int i = 0; i < n; i++) {
+    			  System.out.print(arr[i] + "  ");
+    		}
+  	}
+}
