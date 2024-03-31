b817a8b057d9bc94c4153184beb1462a24dd7ab5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-01 05:14:16 +0530

    code added

---
+++ 14. 1-D Arrays in Java/18_UpdateLastElementOfAnArray.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class UpdateLastElementOfAnArray {
+  	
+  	// TC = O(1), SC = O(1)
+  	public static void updateLastElement(int[] arr, int x) {
+  	  
+  	    arr[arr.length - 1] = x;
+  	}
+  
+    public static void printArray(int[] arr) {
+  
+    		for (int i = 0; i < arr.length; i++) {
+    			  System.out.print(arr[i] + "  ");
+    		}
+    
+    		System.out.println();
+  	}
+  	
+  	public static void main(String[] args) {
+  		
+    		Scanner sc = new Scanner(System.in);
+    		
+    		System.out.print("Enter size of the array: ");
+    		int n = sc.nextInt();
+    		
+    		int[] arr = new int[n];
+    		
+    		System.out.println("Enter array elements: ");
+    		for(int i = 0; i < n; i++) {
+    			  arr[i] = sc.nextInt();
+    		}
+    		
+    		System.out.print("Enter the element: ");
+    		int x = sc.nextInt();
+    		
+    		System.out.print("Before update array elements are: ");
+    		printArray(arr);
+    		
+    		updateLastElement(arr, x);
+    		
+    		System.out.print("After  update array elements are: ");
+    		printArray(arr);
+  	}
+}
