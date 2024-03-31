e3f7c42cbc3c034641d8ce9dd7ed9c84200dd055
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-01 03:38:46 +0530

    code added

---
+++ 14. 1-D Arrays in Java/05_FindArraySize.java
@@ -0,0 +1,7 @@
+public class FindArraySize {
+  	public static void main(String[] args) {
+    		int[] arr = {10, 20, 30 , 40, 50};
+    		
+    		System.out.println("Size of the array = " + arr.length);
+  	}
+}
