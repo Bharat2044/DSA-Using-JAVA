836544c6f3cb2db834f77bd363e192d1dcbfd7ee
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-01 05:31:40 +0530

    code added

--- 14. 1-D Arrays in Java/27_LinearSearch.java
+++ 14. 1-D Arrays in Java/27_LinearSearch.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class LinearSearch {
   
-    //  TC = O(n), SC = O(1)
+    //  Approach - 1: Using Loops => TC = O(n), SC = O(1)
+    /*
     public static int linearSearch(int[] arr, int key) {
         
         for(int i=0; i<arr.length; i++) {
@@ -12,6 +13,24 @@ public class LinearSearch {
         }
         
         return -1;
+    }
+    */
+    
+    // Approach - 2: Using Recursion => TC = O(n), SC = O(n)
+    public static int helper(int[] arr, int i, int key) {
+        if(i == arr.length)
+            return -1;
+        
+        if(arr[i] == key) {
+            return i;
+        }
+        
+        return helper(arr, i + 1, key);
+    }
+    
+    public static int linearSearch(int[] arr, int key) {
+        
+        return helper(arr, 0, key);
     }
   
     public static void main(String[] args) {
