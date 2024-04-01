00094e77e1704fc1b59f027da8927ac96e4b89dd
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-01 19:28:43 +0530

    code added

---
+++ 14. 1-D Arrays in Java/042_LeftRotateByOnePosition.java
@@ -0,0 +1,34 @@
+public class LeftRotateByOnePosition {
+  
+    public static void printArray(int[] arr) {
+      
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + "   ");
+            
+        System.out.println();
+    }
+    
+    // TC = O(n), SC = O(1)
+    public static void LeftRotateByOne(int[] arr) {
+        int n = arr.length;
+        int x = arr[0];
+        
+        for (int i = 1; i < n; i++) {
+            arr[i - 1] = arr[i];
+        }
+        
+        arr[n - 1] = x;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+
+        System.out.println("Before left rotate by 1 position array are:");
+        printArray(arr);
+        
+        LeftRotateByOne(arr);
+
+        System.out.println("\nAfter left rotate by 1 position array are:");
+        printArray(arr);
+    }
+}
