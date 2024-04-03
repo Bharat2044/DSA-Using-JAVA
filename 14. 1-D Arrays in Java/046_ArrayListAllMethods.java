bbe2c7299783ee199b7fff7f4e6bbcaac216a50c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-04-03 19:09:10 +0530

    code added

--- 14. 1-D Arrays in Java/046_ArrayListAllMethods.java
+++ 14. 1-D Arrays in Java/046_ArrayListAllMethods.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ArrayListDemo {
+public class ArrayListAllMethods {
   
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
