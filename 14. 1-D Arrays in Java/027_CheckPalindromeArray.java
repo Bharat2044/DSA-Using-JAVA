c5ddaf67b45eefa2036e8e6be505fb87f5456c8b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-06-15 21:30:14 +0530

    code added

--- 14. 1-D Arrays in Java/027_CheckPalindromeArray.java
+++ 14. 1-D Arrays in Java/027_CheckPalindromeArray.java
@@ -3,28 +3,33 @@
 public class CheckPalindromeArray {
   
     // TC = O(n), SC = O(1)
+    // Method to check if an array is a palindrome
     public static boolean isPalindrome(int[] arr) {
         int st = 0, end = arr.length - 1;
     
-    		while (st < end) { 
-      			if (arr[st] != arr[end]) {
-        				return false;
-      			}
-      
-      			st++;
-      			end--;
-    		}
-    		
-    		return true;
+        // Compare elements from start and end moving towards the center
+        while (st < end) { 
+            if (arr[st] != arr[end]) {
+                // If any elements are not equal, array is not a palindrome
+                return false;
+            }
+            st++;
+            end--;
+        }
+        
+        // If all elements are equal, array is a palindrome
+        return true;
     }
   
-	  public static void main(String[] args) {
-    		int[] arr = { 1, 2, 3, 2, 1 };
+    public static void main(String[] args) {
+        // Sample array to test the palindrome check
+        int[] arr = { 1, 2, 3, 2, 1 };
     
-    		if (isPalindrome(arr)) {
-    	  		System.out.println("Array is Palindrome");
-    		} else {
-      			System.out.println("Array is not Palindrome");
-    		}
+        // Check if the array is a palindrome and print the result
+        if (isPalindrome(arr)) {
+            System.out.println("Array is Palindrome");
+        } else {
+            System.out.println("Array is not Palindrome");
+        }
     }
 }
