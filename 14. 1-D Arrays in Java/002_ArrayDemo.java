7e7b40176feab23d00c2460cc18b3774fc4c8748
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-06-16 19:52:51 +0530

    code added

--- 14. 1-D Arrays in Java/002_ArrayDemo.java
+++ 14. 1-D Arrays in Java/002_ArrayDemo.java
@@ -1,34 +1,40 @@
 public class ArrayDemo {
 
-  	public static void main(String[] args) {
-  
-    		boolean[] arr1 = new boolean[5];
-    		arr1[0] = true;
-    		arr1[2] = false;
-    		arr1[4] = true;
-    		
-    		System.out.print("Array arr1 elements are: ");
-    		for(int i=0; i<arr1.length; i++) {
-    			System.out.print(arr1[i] + "  ");
-    		}
-    		System.out.println("\n");
-    		
-    		
-    		double[] arr2 = {5.5, 6.8, 9.2, 3.5};
-    		
-    		System.out.print("Array arr2 elements are: ");
-    		for(int i=0; i<arr2.length; i++) {
-    			System.out.print(arr2[i] + "  ");
-    		}
-    		System.out.println("\n");
-    		
-    		
-    		char[] arr3 = {'a', 'b', 'c', 'd', 'e'};
-    		
-    		System.out.print("Array arr3 elements are: ");
-    		for(int i=0; i<arr3.length; i++) {
-    			System.out.print(arr3[i] + "  ");
-    		}
-    		System.out.println("\n");
-  	}
+    public static void main(String[] args) {
+
+        // Example 1: Array of boolean values
+        boolean[] arr1 = new boolean[5];
+        arr1[0] = true;
+        arr1[2] = false;
+        arr1[4] = true;
+
+        // Printing elements of arr1
+        System.out.print("Array arr1 elements are: ");
+        for(int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + "  "); // Print each boolean value followed by a space
+        }
+        System.out.println("\n"); // Print a newline for formatting
+
+
+        // Example 2: Array of double values
+        double[] arr2 = {5.5, 6.8, 9.2, 3.5};
+
+        // Printing elements of arr2
+        System.out.print("Array arr2 elements are: ");
+        for(int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + "  "); // Print each double value followed by a space
+        }
+        System.out.println("\n"); // Print a newline for formatting
+
+
+        // Example 3: Array of char values
+        char[] arr3 = {'a', 'b', 'c', 'd', 'e'};
+
+        // Printing elements of arr3
+        System.out.print("Array arr3 elements are: ");
+        for(int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + "  "); // Print each char value followed by a space
+        }
+        System.out.println("\n"); // Print a newline for formatting
+    }
 }
