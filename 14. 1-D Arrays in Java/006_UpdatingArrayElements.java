cd2fc78222ef210c1d92b3f61db424029c49e32a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-06-16 19:46:47 +0530

    code added

--- 14. 1-D Arrays in Java/006_UpdatingArrayElements.java
+++ 14. 1-D Arrays in Java/006_UpdatingArrayElements.java
@@ -1,39 +1,47 @@
 public class UpdatingArrayElements {
 
-  	public static void printArray(int[] arr) {
-  
-    		for (int i = 0; i < arr.length; i++) {
-    			System.out.print(arr[i] + "  ");
-    		}
-    
-    		System.out.println();
-  	}
-  
-  	public static void main(String[] args) {
-    		int[] arr1 = { 10, 20, 30, 40, 50 };
-    
-    		System.out.println("Printing array arr1 elements before updating: ");
-    		printArray(arr1);
-    
-    		arr1[0] = 11; // updaing 1st element
-    		arr1[2] = 33; // updaing 3rd element
-    		arr1[4] = 55; // updaing 5th element
-    
-    		System.out.println("Printing array arr1 elements after updating: ");
-    		printArray(arr1);
-    		
-    
-    		int[] arr2 = { 25, 26, 27, 28, 29 };
-    
-    		System.out.println("\nPrinting array arr2 elements before updating: ");
-    		printArray(arr2);
-    
-    		// updating array arr2
-    		for (int i = 0; i < arr2.length; i++) {
-    			arr2[i] = arr2[i] + 10;
-    		}
-    
-    		System.out.println("Printing array arr2 elements after updating: ");
-    		printArray(arr2);
-  	}
+    // Function to print elements of an integer array
+    public static void printArray(int[] arr) {
+        // Iterate through the array and print each element followed by a space
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
+        }
+      
+        // Print a newline after printing all elements
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Initialize array arr1 with specific elements
+        int[] arr1 = { 10, 20, 30, 40, 50 };
+
+        // Print elements of arr1 before updating
+        System.out.println("Printing array arr1 elements before updating: ");
+        printArray(arr1);
+
+        // Updating elements in arr1
+        arr1[0] = 11; // updating 1st element
+        arr1[2] = 33; // updating 3rd element
+        arr1[4] = 55; // updating 5th element
+
+        // Print elements of arr1 after updating
+        System.out.println("Printing array arr1 elements after updating: ");
+        printArray(arr1);
+
+        // Initialize array arr2 with specific elements
+        int[] arr2 = { 25, 26, 27, 28, 29 };
+
+        // Print elements of arr2 before updating
+        System.out.println("\nPrinting array arr2 elements before updating: ");
+        printArray(arr2);
+
+        // Updating elements in arr2 using a for loop
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = arr2[i] + 10;
+        }
+
+        // Print elements of arr2 after updating
+        System.out.println("Printing array arr2 elements after updating: ");
+        printArray(arr2);
+    }
 }
