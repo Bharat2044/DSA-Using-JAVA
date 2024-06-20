f97f657a76c4410cde3fc995fdd499e7387cf316
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-06-20 14:08:19 +0530

    code added

--- 13. Methods or Functions in Java/26_VarArgsEx4.java
+++ 13. Methods or Functions in Java/26_VarArgsEx4.java
@@ -13,11 +13,11 @@ public class VarArgsEx4 {
     }
     
     public static void main(String[] args) {
-        // demo();  // VarArgsEx4.java:14: error: reference to demo is ambiguous
+        // demo();  // VarArgsEx4.java:16: error: reference to demo is ambiguous
         
         demo(1, 2, 3);  // Calls demo(int ...v) with arguments 1, 2, 3
         demo("Ram", "Mohan", "Deepak");  // Calls demo(String ...v) with arguments "Ram", "Mohan", "Deepak"
         
-        // demo(1, 2, 3, "Deepak"); // VarArgsEx4.java:19: error: no suitable method found for demo(int,int,int,String)
+        // demo(1, 2, 3, "Deepak"); // VarArgsEx4.java:21: error: no suitable method found for demo(int,int,int,String)
     }
 }
