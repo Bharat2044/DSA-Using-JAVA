4aed3941994d697d9eed4e745a258aa4adb6a4bb
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-12-26 17:31:09 +0530

    code added

--- 14. 1-D Arrays in Java/056_SeperateOddEvenNumbers.java
+++ 14. 1-D Arrays in Java/056_SeperateOddEvenNumbers.java
@@ -4,7 +4,7 @@ You are given an integer T denoting the number of test cases. For each test case
 You have to print the odd and even elements of array A in 2 separate lines.
 
 NOTE: Array elements should have the same relative order as in A.
-
+ 
 Problem Constraints:
 1 <= T <= 10
 1 <= |A| <= 10^5
