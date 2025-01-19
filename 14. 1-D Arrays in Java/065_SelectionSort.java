13de5fe858f72ebf7401da5f121e75c164155ea9
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-01-19 18:24:34 +0530

    code added

--- 14. 1-D Arrays in Java/065_SelectionSort.java
+++ 14. 1-D Arrays in Java/065_SelectionSort.java
@@ -7,7 +7,7 @@ public class SelectionSort {
        
         // Iterate through the array elements
         for (int i = 0; i < arr.length; i++) { 
-            
+             
             // Print each element followed by a space 
             System.out.print(arr[i] + " ");
         }
