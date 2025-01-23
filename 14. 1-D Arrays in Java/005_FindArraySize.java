4b84cbf45bf37397698975122864e0c9f4218e41
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-01-23 18:58:13 +0530

    code added

--- 14. 1-D Arrays in Java/005_FindArraySize.java
+++ 14. 1-D Arrays in Java/005_FindArraySize.java
@@ -1,5 +1,6 @@
 public class FindArraySize {
   	public static void main(String[] args) {
+      
         // Initialize an integer array with values
         int[] arr = {10, 20, 30 , 40, 50};
         
