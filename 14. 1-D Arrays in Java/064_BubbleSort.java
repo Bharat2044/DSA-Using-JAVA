67bbd836389f7fab4750726dedb119520d3ee5c3
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-01-25 16:01:07 +0530

    code added

--- 14. 1-D Arrays in Java/064_BubbleSort.java
+++ 14. 1-D Arrays in Java/064_BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort {
         for (int i = 0; i < arr.length; i++) {
             
             // Print each element followed by a space 
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " "); 
         }
         
         // Print a newline after printing all elements
