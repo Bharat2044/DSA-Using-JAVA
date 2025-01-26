ebe2d35d88b092ae392beb72fabd9844472e66b0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-01-26 17:33:33 +0530

    code added

--- 14. 1-D Arrays in Java/066_InsertionSort.java
+++ 14. 1-D Arrays in Java/066_InsertionSort.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BubbleSort {   
      
     // Method to print elements of the array - Time Complexity = O(n), Space Complexity = O(1)
-    public static void printArrayElements(int[] arr) {
+    public static void printArrayElements(int[] arr) { 
           
         // Iterate through the array elements
         for (int i = 0; i < arr.length; i++) {
