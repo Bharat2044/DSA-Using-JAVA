180635b7ab081112709b5448880926bc8fd878f5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-04-28 15:37:13 +0530

    code added

--- Bit Manipulation/03_ReverseBits.java
+++ Bit Manipulation/03_ReverseBits.java
@@ -42,7 +42,7 @@ public class Solution {
             long rev = 0;
 	    
 	    for (int i = 0; i < 32; i++) {
-	        rev <<= 1;
+	        rev <<= 1; 
 
 	        if ((A & (1 << i)) != 0) {
 	            rev |= 1;
