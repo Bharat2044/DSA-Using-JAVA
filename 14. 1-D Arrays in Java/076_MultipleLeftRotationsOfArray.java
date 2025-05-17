15e67778e32a026deb6b15123f3ed43283330f2f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-05-17 16:32:41 +0530

    code added

--- 14. 1-D Arrays in Java/076_MultipleLeftRotationsOfArray.java
+++ 14. 1-D Arrays in Java/076_MultipleLeftRotationsOfArray.java
@@ -1,6 +1,6 @@
 /*
 Multiple left rotations of the array 
- 
+
 Problem Description:
 Given an array of integers A and multiple values in B, which represents the number of times array A needs to be left rotated.
 Find the rotated array for each value and return the result in the from of a matrix where ith row represents the rotated array for the ith value in B.
