7f8f39ea2f301567c1c5063f96cba8fc4bcce8a2
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-07-05 13:57:11 +0530

    code added

--- Bit Manipulation/01_Numberof1Bits.java
+++ Bit Manipulation/01_Numberof1Bits.java
@@ -7,7 +7,7 @@ Write a function that takes an integer and returns the number of 1 bits present 
 Problem Constraints:
 1 <= A <= 10^9
 
-Input Format:
+Input Format: 
 First and only argument contains integer A
 
 Output Format:
