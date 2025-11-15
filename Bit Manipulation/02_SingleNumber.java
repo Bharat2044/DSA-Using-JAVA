f3a94122393baee5b06648494b42093a6ae2b784
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-11-15 15:55:01 +0530

    code added

--- Bit Manipulation/02_SingleNumber.java
+++ Bit Manipulation/02_SingleNumber.java
@@ -1,7 +1,7 @@
 /* 
 Single Number
 
-Problem Description:
+Problem Description: 
 Given an array of integers A, every element appears twice except for one. Find that integer that occurs once.
 NOTE: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
