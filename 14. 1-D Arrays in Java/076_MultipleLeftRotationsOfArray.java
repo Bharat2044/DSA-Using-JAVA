44bdf1f2a08922163452c779c76303e502bf7777
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-11-29 08:19:43 +0530

    code added

--- 14. 1-D Arrays in Java/076_MultipleLeftRotationsOfArray.java
+++ 14. 1-D Arrays in Java/076_MultipleLeftRotationsOfArray.java
@@ -6,7 +6,7 @@ Given an array of integers A and multiple values in B, which represents the numb
 Find the rotated array for each value and return the result in the from of a matrix where ith row represents the rotated array for the ith value in B.
 
 Problem Constraints: 
-1 <= length of both arrays <= 2000 -10^9 <= A[i] <= 10^9 0 <= B[i] <= 2000 
+1 <= length of both arrays <= 2000 -10^9 <= A[i] <= 10^9 0 <= B[i] <= 2000
 
 Input Format: 
 The first argument given is the integer array A.
