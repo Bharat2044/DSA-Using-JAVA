99229184b04fcc7af52389ba3558cda5cc5e629a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2025-12-27 14:12:45 +0530

    code added

--- Bit Manipulation/01_Numberof1Bits.java
+++ Bit Manipulation/01_Numberof1Bits.java
@@ -3,7 +3,7 @@ Number of 1 Bits
 
 Problem Description: 
 Write a function that takes an integer and returns the number of 1 bits present in its binary representation.
-
+ 
 Problem Constraints: 
 1 <= A <= 10^9 
  
