e9b353120afa79242f20e9afb6dc60d6b24ab3f8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2026-01-20 16:44:25 +0530

    code added

--- 11. Loops in Java/09. Loops Programs/12_CountFactors.java
+++ 11. Loops in Java/09. Loops Programs/12_CountFactors.java
@@ -33,40 +33,68 @@ Factors of 10 are 1, 2, 5 and 10.
 */
 
 
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner class for input
 
 public class CountFactors {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
+
+        Scanner sc = new Scanner(System.in); // Create Scanner object
+        int n = sc.nextInt(); // Read input number
+
         /*
-        // 1st Approach => TC = O(n), SC = O(1)
-        int count = 0;
-      
-        for(int i=1; i<=n; i++) {
-            if(n % i == 0) {
-                count++;
+        ----------------------------------------------------
+        1st Approach: Brute Force
+        ----------------------------------------------------
+        Idea:
+        - Check every number from 1 to n
+        - Count how many numbers divide n
+        ----------------------------------------------------
+        Time Complexity  : O(n)
+        Space Complexity : O(1)
+        ----------------------------------------------------
+        */
+        /*
+        int count = 0; // Variable to store factor count
+
+        for (int i = 1; i <= n; i++) { // Loop from 1 to n
+            if (n % i == 0) { // If i divides n
+                count++; // Increase factor count
             }
         }
-            
-        System.out.println(count);
+
+        System.out.println(count); // Print total factors
         */
-        
-        
-        // 2nd Approach => TC = O(sqrt(n)), SC = O(1)
-        int count = 0;
-      
-        for(int i=1; i*i<=n; i++) {
-            if(n % i == 0) {
-                count++;
-                
-                if(i * i != n) {
-                  count++;
+
+
+        /*
+        ----------------------------------------------------
+        2nd Approach: Optimized using sqrt(n)
+        ----------------------------------------------------
+        Idea:
+        - Factors come in pairs (i, n / i)
+        - Iterate only till sqrt(n)
+        - Count both factors in one step
+        ----------------------------------------------------
+        Time Complexity  : O(sqrt(n))
+        Space Complexity : O(1)
+        ----------------------------------------------------
+        */
+
+        int count = 0; // Stores total number of factors
+
+        for (int i = 1; i * i <= n; i++) { // Loop till sqrt(n)
+
+            if (n % i == 0) { // If i is a factor
+                count++; // Count factor i
+
+                if (i * i != n) { // If paired factor is different
+                    count++; // Count factor n / i
                 }
             }
         }
-            
-        System.out.println(count);
+
+        System.out.println(count); // Print total number of factors
+        sc.close(); // Close Scanner
     }
 }
